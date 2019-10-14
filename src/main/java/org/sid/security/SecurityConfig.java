225a3e6aad7745f9f01b0fdc35f5b4593a608cc7
B65473 <B65473@LAPTOP-572BDHV9>
2019-10-14 17:13:50 +0200

    5_attribute_userRights_inMethod_http

--- src/main/java/org/sid/security/SecurityConfig.java
+++ src/main/java/org/sid/security/SecurityConfig.java
@@ -16,6 +16,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-
+		http.formLogin();
+		//une requete http dans l'url "/index" nécessaite une authetification avec un utilisateur avec un rôle "USER"
+		http.authorizeRequests().antMatchers("/index").hasRole("USER");
+		//une requete http dans l'url "/form","/save","/edit","/delete" nécessaite une authetification avec un utilisateur avec un rôle "ADMIN"
+		http.authorizeRequests().antMatchers("/form","/save","/edit","/delete").hasRole("ADMIN");
 	}
 }
