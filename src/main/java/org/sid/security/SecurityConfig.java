bf84c6eee6320c37f745913bfe56d1f68b32557d
B65473 <B65473@LAPTOP-572BDHV9>
2019-10-15 11:46:36 +0200

    10_gestionMultiCompte_SecuConf

--- src/main/java/org/sid/security/SecurityConfig.java
+++ src/main/java/org/sid/security/SecurityConfig.java
@@ -19,13 +19,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		
 		http.formLogin();
-		//une requete http dans l'url "/index" nécessaite une authetification avec un utilisateur avec un rôle "USER"
+		
+		/*une requete http dans l'url "/index" nécessaite une authetification avec un utilisateur avec un rôle "USER"
 		http.authorizeRequests().antMatchers("/index").hasRole("USER");
 		//une requete http dans l'url "/form","/save","/edit","/delete" nécessaite une authetification avec un utilisateur avec un rôle "ADMIN"
 		http.authorizeRequests().antMatchers("/form","/save","/edit","/delete").hasRole("ADMIN");
 		//acces non authorisé à la page
+		http.exceptionHandling().accessDeniedPage("/403");*/
+			
+		//gestion de plusieurs utilisateurs
+		//necessite le rôle "user"
+		http.authorizeRequests().antMatchers("/user/*").hasRole("USER");	
+		//necessite le rôle "admin"
+		http.authorizeRequests().antMatchers("/admin/*").hasRole("ADMIN");
+		//acces non authorisé à la page
 		http.exceptionHandling().accessDeniedPage("/403");
+		
 	}
     @Bean
     public PasswordEncoder passwordEncoder(){
